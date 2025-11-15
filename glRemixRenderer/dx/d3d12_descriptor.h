55af74d62f93ade2f75be1f883323d782679947c
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2025-11-15 12:09:40 -0500

    make alloc member private in descriptor table

--- glRemixRenderer/dx/d3d12_descriptor.h
+++ glRemixRenderer/dx/d3d12_descriptor.h
@@ -2,13 +2,15 @@
 
 namespace glRemix::dx
 {
-struct D3D12DescriptorHeap;
+class D3D12DescriptorHeap;
 
 struct D3D12DescriptorTable
 {
-    D3D12MA::VirtualAllocation alloc{};
     UINT64 offset = 0;  // Offset from start of heap in number of descriptors
     size_t count = 0;   // Number of descriptors in this table
     D3D12DescriptorHeap* heap = nullptr;
+private:
+    D3D12MA::VirtualAllocation alloc{};
+    friend class D3D12DescriptorHeap;
 };
 }  // namespace glRemix::dx
