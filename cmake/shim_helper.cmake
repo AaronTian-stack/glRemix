d2d6b041ad5fbaf79c69252d84899e36396e2059
Amy Liu <mialana@users.noreply.github.com>
2025-11-29 02:05:25 -0500

    ci: set vs working directory to exe path

--- cmake/shim_helper.cmake
+++ cmake/shim_helper.cmake
@@ -34,10 +34,11 @@ function(set_shim_target_properties ${target} ${platform})
     set_property(DIRECTORY ${REPO_ROOT} PROPERTY VS_STARTUP_PROJECT ${target}) # set as startup proj
 
     if (GLREMIX_SHIM_DEBUGGER_EXE_PATH) # set launch executable within visual studio
+        cmake_path(GET GLREMIX_SHIM_DEBUGGER_EXE_PATH PARENT_PATH GLREMIX_SHIM_DEBUGGER_DIR)
         set_target_properties(${target} PROPERTIES
             VS_DEBUGGER_COMMAND            "${GLREMIX_SHIM_DEBUGGER_EXE_PATH}"
             VS_DEBUGGER_COMMAND_ARGUMENTS  ""
-            VS_DEBUGGER_WORKING_DIRECTORY  "${CMAKE_CURRENT_SOURCE_DIR}"
+            VS_DEBUGGER_WORKING_DIRECTORY  "${GLREMIX_SHIM_DEBUGGER_DIR}"
         )
     endif()
 endfunction()
